146dc38d1bf315b708e9b614df8a7a72917d4167
EthanMoskowitz <ethanmoskowitz0@gmail.com>
2023-10-03 14:28:45 -0400

    Adding connection to Mongo db

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,24 +1,10 @@
-import { config as dotenvConfig } from 'dotenv';
-import mongoose from 'mongoose';
 import User from '../models/user.js';
+import Connection from '../db/connection.js';
 
 export default class UserController {
     static async getAllUsers(req, res) {
-        dotenvConfig();
 
-        const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_CLUSTER } = process.env;
-        const DATABASE_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_CLUSTER}.xtdufxk.mongodb.net/?retryWrites=true&w=majority`;
-
-        mongoose.connect(
-            DATABASE_URL,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                maxPoolSize: 50,
-                socketTimeoutMS: 2500
-            }
-        );
-
+        await Connection.open("users");
         const users = [];
         try {
             for await (const doc of User.find()) {
---
+++ db/connection.js
@@ -0,0 +1,24 @@
+import { config as dotenvConfig } from 'dotenv';
+import mongoose from 'mongoose';
+
+export default class Connection {
+    static async open(db) {
+        dotenvConfig();
+
+        const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_CLUSTER } = process.env;
+        const DATABASE_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_CLUSTER}.xtdufxk.mongodb.net/?retryWrites=true&w=majority`;
+
+        mongoose.connect(
+            DATABASE_URL,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                maxPoolSize: 50,
+                socketTimeoutMS: 2500,
+                dbName: db
+            }
+        );
+
+        return mongoose.connection;
+    }
+}
